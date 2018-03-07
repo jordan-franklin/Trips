a49a4036e1928a8a60b841fe794382028c71049e
Becky Henderson <becky@macpractice.com>
2018-03-07 08:36:39 -0600

    Ripple

--- app/src/main/java/com/rebeccahenderson/trips/TripsListActivity.kt
+++ app/src/main/java/com/rebeccahenderson/trips/TripsListActivity.kt
@@ -3,12 +3,20 @@ package com.rebeccahenderson.trips
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_trips.*
 
 class Trips : AppCompatActivity() {
 
     lateinit private var staggeredLayoutManager: StaggeredGridLayoutManager
     lateinit private var adapter: TripsListAdapter
+
+    private val onItemClickListener = object : TripsListAdapter.OnItemClickListener {
+        override fun onItemClick(view: View, position: Int) {
+//            Toast.makeText(this@MainActivity, "Clicked " + position, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,6 @@ class Trips : AppCompatActivity() {
 
         adapter = TripsListAdapter(this)
         tripList.adapter = adapter
+        adapter.setOnItemClickListener(onItemClickListener)
     }
 }
--- app/src/main/java/com/rebeccahenderson/trips/TripsListAdapter.kt
+++ app/src/main/java/com/rebeccahenderson/trips/TripsListAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.row_trip.view.*
 
 /**
@@ -12,6 +13,8 @@ import kotlinx.android.synthetic.main.row_trip.view.*
  */
 
 class TripsListAdapter(private var context: Context) : RecyclerView.Adapter<TripsListAdapter.ViewHolder>() {
+
+	lateinit var itemClickListener: OnItemClickListener
 
 	override fun getItemCount() = TripsData.tripsList().size
 
@@ -26,6 +29,19 @@ class TripsListAdapter(private var context: Context) : RecyclerView.Adapter<Trip
 //		Picasso.with(context).load(place.getImageResourceId(context)).into(holder?.itemView?.placeImage)
 	}
 
-	inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+	inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+		init {
+			itemView.tripHolder.setOnClickListener(this)
+		}
+
+		override fun onClick(view: View) = itemClickListener.onItemClick(itemView, adapterPosition)
+	}
+
+	interface OnItemClickListener {
+		fun onItemClick(view: View, position: Int)
+	}
+
+	fun setOnItemClickListener(itemClickListener: OnItemClickListener) {
+		this.itemClickListener = itemClickListener
 	}
 }
